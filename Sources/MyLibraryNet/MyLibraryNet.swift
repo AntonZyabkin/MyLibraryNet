1033dabe3ed6f1e03ec7a645b3f6879323ba6e0c
Anatan <zyabkinantonv@gmail.com>
2023-09-29 12:07:17 +0300

    public

--- Sources/MyLibraryNet/MyLibraryNet.swift
+++ Sources/MyLibraryNet/MyLibraryNet.swift
@@ -1,73 +1,71 @@
-protocol NetworkServicerPotocol {
-		func request<T>(_ request: Urlable, complition: @escaping (Result<T, Error>) -> Void) where T: Decodable
-		
+import Foundation
+public protocol NetworkServicerPotocol {
+	func request<T>(_ request: Urlable, complition: @escaping (Result<T, Error>) -> Void) where T: Decodable
+	
 }
 
-final class NetworkServiceByURLSession {
-		private let decoderService: DecoderServicable
-		init(decoderService: DecoderServicable) {
-				self.decoderService = decoderService
-		}
+public final class NetworkServiceByURLSession {
+	private let decoderService: DecoderServicable
+	init(decoderService: DecoderServicable) {
+		self.decoderService = decoderService
+	}
 }
 
 extension NetworkServiceByURLSession: NetworkServicerPotocol {
-		func request<T>(_ request: Urlable, complition: @escaping (Result<T, Error>) -> Void) where T: Decodable {
-				func complitionHandler(_ result: Result<T, Error>) {
-						DispatchQueue.main.async {
-								complition(result)
-						}
-				}
-				DispatchQueue.global(qos: .userInitiated).async {  [weak self] in
-						guard let self = self else {
-								return
-						}
-						guard let url = URL(string: request.urlString) else { return }
-						let urlRequest = URLRequest(url: url)
-						URLSession.shared.dataTask(with: urlRequest, completionHandler: { data, response, error in
-								if let error = error {
-										complition(.failure(error))
-								}
-								if let data = data {
-										self.decoderService.decode(data, complition: complition)
-								}
-						}).resume()
-				}
+	public func request<T>(_ request: Urlable, complition: @escaping (Result<T, Error>) -> Void) where T: Decodable {
+		func complitionHandler(_ result: Result<T, Error>) {
+			DispatchQueue.main.async {
+				complition(result)
+			}
 		}
+		DispatchQueue.global(qos: .userInitiated).async {  [weak self] in
+			guard let self = self else {
+				return
+			}
+			guard let url = URL(string: request.urlString) else { return }
+			let urlRequest = URLRequest(url: url)
+			URLSession.shared.dataTask(with: urlRequest, completionHandler: { data, response, error in
+				if let error = error {
+					complition(.failure(error))
+				}
+				if let data = data {
+					self.decoderService.decode(data, complition: complition)
+				}
+			}).resume()
+		}
+	}
 }
-import Foundation
-
-
-protocol DecoderServicable{
-		func decode<T: Decodable>(_ data: Data, complition: @escaping (Result<T, Error>) -> Void)
-		func encode<T: Encodable>(_ data: T, complition: @escaping (Result<Data, Error>) -> Void)
+public protocol DecoderServicable{
+	func decode<T: Decodable>(_ data: Data, complition: @escaping (Result<T, Error>) -> Void)
+	func encode<T: Encodable>(_ data: T, complition: @escaping (Result<Data, Error>) -> Void)
 }
 
-final class DecoderService{
-		private let jsonDecoder = JSONDecoder()
-		private let jsonEncoder = JSONEncoder()
+public final class DecoderService{
+	private let jsonDecoder = JSONDecoder()
+	private let jsonEncoder = JSONEncoder()
 }
 
 extension DecoderService: DecoderServicable {
-		func decode<T: Decodable>(_ data: Data, complition: @escaping (Result<T, Error>) -> Void) {
-				do {
-						let result = try self.jsonDecoder.decode(T.self, from: data)
-						complition(.success(result))
-				} catch  {
-						complition(.failure(error))
-				}
+	public func decode<T: Decodable>(_ data: Data, complition: @escaping (Result<T, Error>) -> Void) {
+		do {
+			let result = try self.jsonDecoder.decode(T.self, from: data)
+			complition(.success(result))
+		} catch  {
+			complition(.failure(error))
 		}
-		
-		func encode<T: Encodable>(_ data: T, complition: @escaping (Result<Data, Error>) -> Void) {
-				do {
-						let result = try self.jsonEncoder.encode(data)
-						complition(.success(result))
-				} catch  {
-						complition(.failure(error))
-				}
+	}
+	
+	public func encode<T: Encodable>(_ data: T, complition: @escaping (Result<Data, Error>) -> Void) {
+		do {
+			let result = try self.jsonEncoder.encode(data)
+			complition(.success(result))
+		} catch  {
+			complition(.failure(error))
 		}
+	}
 }
 
 
-protocol Urlable {
-		var urlString: String { get }
+public protocol Urlable {
+	var urlString: String { get }
 }
